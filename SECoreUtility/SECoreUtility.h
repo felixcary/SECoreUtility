3dc18f92a62f9c5e3741bd414baa58615623a623
Felix <felix.cary@gmail.com>
2020-06-29 15:01:34 +0700

    Add secoreutility

---
+++ SECoreUtility/SECoreUtility.h
@@ -0,0 +1,19 @@
+//
+//  SECoreUtility.h
+//  SECoreUtility
+//
+//  Created by Felix on 29/06/20.
+//  Copyright © 2020 SweetEscape. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SECoreUtility.
+FOUNDATION_EXPORT double SECoreUtilityVersionNumber;
+
+//! Project version string for SECoreUtility.
+FOUNDATION_EXPORT const unsigned char SECoreUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SECoreUtility/PublicHeader.h>
+
+
